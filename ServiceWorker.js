0f060f623290825310331bcb9a60d69512232c84
Dicky Dwi Darmawan <darmadicky17@gmail.com>
2023-07-16 14:41:59 +0700

    second commit

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Firebase Test-FirebaseDatabaseTest-1.0";
+const contentToCache = [
+    "Build/[WebGL] TestFirebase.loader.js",
+    "Build/[WebGL] TestFirebase.framework.js",
+    "Build/[WebGL] TestFirebase.data",
+    "Build/[WebGL] TestFirebase.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
